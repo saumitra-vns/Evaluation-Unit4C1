e4da9e6f1f93756bd2fcf37d765071f432575759
saumitra-vns <97459938+saumitra-vns@users.noreply.github.com>
2022-03-07 12:11:58 +0530

    use of logger

--- index.js
+++ index.js
@@ -4,22 +4,34 @@ const { get } = require("http")
 const app = express()
 
 
-app.get("/books", (req,res)=>{
+app.get("/books",logger, (req,res)=>{
 
     return res.send({ route: "/books"})
 })
 
-app.get("/libraries", (req,res)=>{
+app.get("/libraries",logger,checkPermission, (req,res)=>{
 
     return res.send({ route: "/libraries", permission: true})
 })
 
-app.get("/authors", (req,res)=>{
+app.get("/authors",logger,checkPermission, (req,res)=>{
 
     return res.send( { route: "/authors", permission: true})
 })
 
 
+function logger(req,res,next)
+{
+    console.log("this is logger")
+    next()
+}
+
+function checkPermission(req, res, next){
+
+    console.log("This is check logger")
+
+    next()
+}
 
 app.listen (5500, (req,res)=>{
     console.log("listining on port 5500")
